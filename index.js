17226b073366d7633335ebba56f42af0b0e324d8
Mister-Kay <ericbanker.12@gmail.com>
2017-05-20 13:28:14 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+// Formats messages - made by teralove: https://github.com/teralove
+const format = require('./format.js');
+
+// Always block messages that start with "!"
+module.exports = function FpsUtils(dispatch) {
+	
+	// Hooks chat you send
+	dispatch.hook('C_CHAT', 1, {order: 100}, () => {
+		let message = format.stripTags(event.message)
+		// If message starts with "!", do not send message to server
+		if (message.startsWith('!')) {return false}
+	})
+}
