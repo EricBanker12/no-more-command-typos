90a889b1542fe6b06e137b03736ddcbddb4067d9
Mister-Kay <ericbanker.12@gmail.com>
2017-05-20 17:48:21 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,41 @@
-// Formats messages - made Meishu: https://github.com/meishuu
-const format = require('./string.js');
+// Formats messages - made by Meishu: https://github.com/meishuu Source: https://github.com/baldera-mods/slash/blob/master/string.js
+const format = require('./string.js')
 
 // Always block messages that start with "!"
-module.exports = function noMoreCommandTypos(dispatch) {
+module.exports = function FpsUtils(dispatch) {
 	
 	// Hooks chat you send
 	dispatch.hook('C_CHAT', 1, {order: 100}, event => {
+		
+		// remove font formatting from message
 		let message = format.stripTags(event.message)
-		// If message starts with "!", do not send message to server
+		
+		// If message starts with "\\!", do send the message
+		if (message.startsWith('\\!')) {
+			event.message = event.message.replace('\\!', '!')
+			return true
+		}
+		
+		// If message starts with "!", do NOT send message to server
 		if (message.startsWith('!')) {return false}
 	})
+	
+	// Hooks whispers you send
+	dispatch.hook('C_WHISPER', 1, {order: 100}, event => {
+		
+		// remove font formatting from message
+		let message = format.stripTags(event.message)
+		
+		// If target starts with "!", do NOT send the message
+		if (event.target.startsWith('!')) {return false}
+		
+		// If message starts with "\\!", do send the message
+		if (message.startsWith('\\!')) {
+			event.message = event.message.replace('\\!', '!')
+			return true
+		}
+		
+		// If message starts with "!", do NOT send message
+		if (message.startsWith('!')) {return false}
+	})	
 }
